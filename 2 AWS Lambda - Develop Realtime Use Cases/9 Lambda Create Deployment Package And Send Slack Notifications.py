288f37478576dd0e38dff082102771d3be1b0777
Ainoko Alex Idachaba <click2alex@gmail.com>
2023-11-06 15:25:34 +0100

    create deployment package and send slack notification

---
+++ 2 AWS Lambda - Develop Realtime Use Cases/9 Lambda Create Deployment Package And Send Slack Notifications.py
@@ -0,0 +1,11 @@
+import os
+import json
+import requests
+
+slack_web_hook = os.environ('SLACK_WEBHOOK')
+
+def send_slack(event, context):
+    print(str(event))
+    slack_message = {'text': 'EC2 Instance Stopped'}
+    resp = requests.post(slack_web_hook, data=json.dumps(slack_message))
+    return resp
